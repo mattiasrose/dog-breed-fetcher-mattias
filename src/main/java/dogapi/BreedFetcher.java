c3af52f9c03cf0bcadac4b960a68dee50f2f93df
fentonfarrell <mattias.rose@mail.utoronto.ca>
2025-10-17 20:42:19 -0400

    Fix BreedFetcher

--- src/main/java/dogapi/BreedFetcher.java
+++ src/main/java/dogapi/BreedFetcher.java
@@ -16,7 +16,7 @@ public interface BreedFetcher {
     List<String> getSubBreeds(String breed) throws BreedNotFoundException;
 
     // a class defined in an interface is public AND static
-    class BreedNotFoundException extends RuntimeException {
+    class BreedNotFoundException extends Exception {
         public BreedNotFoundException(String breed) {
             super("Breed not found: " + breed);
         }
